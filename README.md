8475bcaa8116f7e784e13b6f0e8d6f15b92f8ac9
Miikka Koskinen <miikka.koskinen@iki.fi>
2018-07-06 10:24:33 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# ci-release-test
+
+Experiments in release automation.
+
+## License
+
+Copyright © 2018 Miikka Koskinen.
+
+Distributed under the Eclipse Public License either version 1.0 or (at
+your option) any later version.
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject org.clojars.miikka/ci-release-test "0.1.0-SNAPSHOT"
+  :description "Experiments with release automation"
+  :url "https://github.com/miikka/ci-release-test"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-repositories [["releases" :clojars]]
+  :dependencies [[org.clojure/clojure "1.9.0"]])
