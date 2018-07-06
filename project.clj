fd8a2030c0343b99c8b1ba85bdf55d1311a8d849
Miikka Koskinen <miikka.koskinen@iki.fi>
2018-07-06 11:08:23 +0300

    Add Clojars credentials

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.miikka/ci-release-test "0.1.0-SNAPSHOT"
+(defproject org.clojars.oispa/ci-release-test "0.1.0-SNAPSHOT"
   :description "Experiments with release automation"
   :url "https://github.com/miikka/ci-release-test"
   :license {:name "Eclipse Public License"
--- scripts/encrypt-secrets.sh
+++ scripts/encrypt-secrets.sh
@@ -3,4 +3,4 @@
 set -euo pipefail
 
 tar cfv .circleci/secrets.tar secrets
-gpg -c --batch --yes --passphrase "$1" .circleci/secrets.tar 
+gpg -c --batch --yes --passphrase "$1" .circleci/secrets.tar
