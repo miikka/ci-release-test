23b1e969acfc7653439615c70d882a392bdaea0d
Build automation <miikka.koskinen+ci@iki.fi>
2018-07-06 10:01:29 +0000

    Version 0.3.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.oispa/ci-release-test "0.3.0"
+(defproject org.clojars.oispa/ci-release-test "0.3.1-SNAPSHOT"
   :description "Experiments with release automation"
   :url "https://github.com/miikka/ci-release-test"
   :license {:name "Eclipse Public License"
